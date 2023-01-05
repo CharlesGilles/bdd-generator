2b0022055484bf326cde077a7f9855021c587fc8
Charles Gilles <charles.gilles@axa.fr>
2023-01-05 18:13:28 +0100

    test(Vitest): Ajout d'un projet example avec vitest

---
+++ examples/ecmascript-vitest/src/__features__/basic-with-imports/basic-scenarios.steps.tsx
@@ -0,0 +1,60 @@
+import { defineFeature, loadFeature } from 'jest-cucumber';
+import { PasswordValidator } from '../../password-validator';
+const feature = loadFeature('./src/__features__/basic-with-imports/basic-scenarios.feature');
+
+defineFeature(feature, (defineScenario) => {
+	let scenarioContext;
+    
+	beforeEach(() => {
+		scenarioContext = {};
+    });
+
+	beforeEach(() => {
+		  scenarioContext.yolo = 10;
+	});
+
+	beforeEach(() => {
+		  scenarioContext.x = 10;
+	});
+
+	afterEach(() => {
+		  scenarioContext.x = undefined;
+	});
+
+	function IHavePreviouslyCreatedAPassword(defineMethod){
+		defineMethod('I have previously created a password', () => {
+			let passwordValidator = new PasswordValidator();
+			scenarioContext.accessGranted = false;
+			passwordValidator.setPassword("1234");
+			scenarioContext.passwordValidator = passwordValidator;
+		});
+	}
+
+	function IEnterMyPasswordCorrectly(defineMethod){
+		defineMethod('I enter my password correctly', () => {
+			scenarioContext.accessGranted = scenarioContext.passwordValidator.validatePassword("1234");
+		});
+	}
+
+	function IShouldBeGrantedAccess(defineMethod){
+		defineMethod('I should be granted access', () => {
+			expect(scenarioContext.accessGranted).toBe(true);
+		});
+	}
+
+	function YOLO(defineMethod){
+		defineMethod('YOLO', () => {
+			expect(scenarioContext.yolo).toBe(10);
+		});
+	}
+
+	defineScenario('Entering a correct password', ({ given, when, then}) => {
+		IHavePreviouslyCreatedAPassword(given);
+		IEnterMyPasswordCorrectly(when);
+		IShouldBeGrantedAccess(then);
+	});
+
+	defineScenario('test tolo', ({ given, when, then}) => {
+		YOLO(then);
+	});
+});
