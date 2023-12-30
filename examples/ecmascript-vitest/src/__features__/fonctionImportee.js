39872472fb96252f3fee697f6eceb86d3ebc745d
herodeaths <39561771+herodeaths@users.noreply.github.com>
2023-12-30 13:00:00 +0100

    test(ImportsParDefaut): ajout de l'exemple avec un import par défaut

--- examples/ecmascript-vitest/src/__features__/fonctionImportee.js
+++ examples/ecmascript-vitest/src/__features__/fonctionImportee.js
@@ -1,3 +1,7 @@
 export function fonctionImportee(){
     // cette fonction permet de tester le bon fonctionnement des imports relatifs dans les fichiers steps
 }
+
+const x = 1;
+
+export default x;
